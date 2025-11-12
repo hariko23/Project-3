5a0d398984fba3c48c2778a1c8e84336a5dc1c7e
Thomas <thomasdocog@gmail.com>
2025-11-12 12:56:07 -0600

    Fixed changing order status

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,5 +4,9 @@ const orderController = require('../controllers/orderController');
 
 router.get('/', orderController.getAllOrders);
 router.post('/', orderController.createOrder);
+// Specific routes before parameterized routes
+router.get('/items/:orderItemId', orderController.getOrderItemById);
+router.patch('/items/:orderItemId/complete', orderController.markOrderItemComplete);
+router.get('/:orderId/items', orderController.getOrderItems);
 
 module.exports = router;
